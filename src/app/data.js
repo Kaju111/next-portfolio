cc4165f6a5a516234165dd784071e7eca44f51b9
kaju111 <saikiakaju275@gmail.com>
2024-05-01 20:40:52 +0530

    almost done

--- src/app/data.js
+++ src/app/data.js
@@ -19,73 +19,57 @@ Websites:
 export const projectsData = [
   {
     id: 1,
-    name: "EcoTracker",
-    description: "Track your carbon footprint",
-    date: "2022-08-15",
-    demoLink: "https://ecotracker.example.com",
+    name: "E-COMMERCE WEB",
+    description:
+      "Built a Node.js-powered e-commerce platform featuring user authentication, product management, and secure payment integration via Stripe. Included an exclusive admin dashboard for streamlined product and order management.",
+    date: "2023-08-15",
+    demoLink: "https://server-frontend.vercel.app/",
   },
   {
     id: 2,
-    name: "ArtGallery Online",
-    description: "Digital art showcase platform",
-    date: "2022-06-20",
-    demoLink: "https://artgalleryonline.example.com",
+    name: "DRAWING",
+    description:
+      "Developed a drawing website using React with features like drawing tools, shapes, text, undo/redo, and local storage integration. Continuing to enhance functionality for better user experience.",
+    date: "2024-01-20",
+    demoLink: "https://blank-eight.vercel.app/",
   },
   {
     id: 3,
-    name: "BudgetPlanner",
-    description: "Plan and track expenses",
-    date: "2022-09-10",
-    demoLink: "https://budgetplanner.example.com",
+    name: "SOCIAL MEDIA",
+    description:
+      "Developed a MERN stack social media platform with Cloudinary integration for photo upload/delete. Features include login, logout, create account, comment management, follow/unfollow, user profiles.",
+    date: "2023-04-10",
+    demoLink:
+      "https://www.linkedin.com/posts/kaju-saikia-2706422a9_tech-react-node-mongo-express-cloudinary-activity-7167053279770624000-jOHR?utm_source=share&utm_medium=member_desktop",
   },
   {
     id: 4,
-    name: "HealthBeat",
-    description: "Monitor heart rate zones",
-    date: "2022-05-30",
-    demoLink: "https://healthbeat.example.com",
+    name: "DASHBOARD",
+    description:
+      "Simple e-com dashboard app Orders, Employees, Customers, Calender, Kanban, Editor, Color-Picker all sites available and Dark mode feafures.",
+    date: "2023-01-30",
+    demoLink: "https://shoppy-dashboard-seven.vercel.app/",
   },
   {
     id: 5,
-    name: "RecipeFinder",
-    description: "Discover new recipes",
+    name: "CLONE",
+    description: "A landing page for a chai walla",
     date: "2022-07-12",
-    demoLink: "https://recipefinder.example.com",
+    demoLink: "https://mbachaiwala-psi.vercel.app/",
   },
   {
     id: 6,
-    name: "JourneyLogger",
-    description: "Log your travels",
-    date: "2022-10-01",
-    demoLink: "https://journeylogger.example.com",
+    name: "NIKE STORE",
+    description: "A landing page for a Shose company",
+    date: "2023-2-01",
+    demoLink: "https://nike-store-saikiakaju275-gmailcom.vercel.app/",
   },
   {
     id: 7,
-    name: "StudyBuddy",
-    description: "Collaborative learning platform",
-    date: "2022-04-18",
-    demoLink: "https://studybuddy.example.com",
-  },
-  {
-    id: 8,
-    name: "TechTalk",
-    description: "Tech news aggregator",
-    date: "2022-11-05",
-    demoLink: "https://techtalk.example.com",
-  },
-  {
-    id: 9,
-    name: "FitTrack",
-    description: "Fitness and workout tracker",
-    date: "2022-03-22",
-    demoLink: "https://fittrack.example.com",
-  },
-  {
-    id: 10,
-    name: "MindfulMoments",
-    description: "Meditation and mindfulness app",
-    date: "2022-02-14",
-    demoLink: "https://mindfulmoments.example.com",
+    name: "PORTFOLIO",
+    description: "My old portfolio",
+    date: "2023-06-18",
+    demoLink: "https://kaju.vercel.app/",
   },
 ];
 
@@ -96,19 +80,19 @@ export const BtnList = [
   { label: "Contact", link: "/contact", icon: "contact", newTab: false },
   {
     label: "Github",
-    link: "https://www.github.com/codebucks27",
+    link: "https://github.com/Kaju111",
     icon: "github",
     newTab: true,
   },
   {
     label: "LinkedIn",
-    link: "https://www.linkedin.com/in/codebucks",
+    link: "https://www.linkedin.com/in/kaju-saikia-2706422a9/",
     icon: "linkedin",
     newTab: true,
   },
   {
     label: "X",
-    link: "https://www.x.com/code_bucks",
+    link: "https://twitter.com/KajuSaikia99107",
     icon: "twitter",
     newTab: true,
   },
@@ -119,5 +103,3 @@ export const BtnList = [
     newTab: true,
   },
 ];
-
-
