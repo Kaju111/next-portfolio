48ea48a1d385323d485c2a991bd2979b44b8f3a3
kaju111 <saikiakaju275@gmail.com>
2024-05-03 16:43:23 +0530

    done

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,19 +1,6 @@
 import React from "react";
 import ItemLayout from "./ItemLayout";
 import Link from "next/link";
-import { Github, Linkedin } from "lucide-react";
-
-const getIcon = (icon) => {
-  switch (icon) {
-    case "github":
-      return <Github className="w-full h-auto" strokeWidth={1.5} />;
-    case "linkedin":
-      return <Linkedin className="w-full h-auto" strokeWidth={1.5} />;
-
-    default:
-      return <Home className="w-full h-auto" strokeWidth={1.5} />;
-  }
-};
 
 const AboutDetails = () => {
   return (
